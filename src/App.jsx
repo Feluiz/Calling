7aea1e0d6101879ac41e3dda68045ed3f0e29905
Feluiz <138745247+Feluiz@users.noreply.github.com>
2025-05-13 23:01:18 -0600

    update: styled list items and added effects. Also refined some processing and added data.

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,18 @@ function App() {
       "https://cdn3.iconfinder.com/data/icons/catcommerce-ginger/120/search-512.png",
     key: "",
     description: "",
+    perks: {
+      adaptability: '',
+      affection_level: '',
+      stranger_friendly: '',
+      social_needs: '',
+    },
   });
 
   const fetchDino = () => {
     axios
       .get(
-        "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=10",
+        "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=9",
         {
           headers: {
             "x-api-key":
@@ -36,6 +42,12 @@ function App() {
             image: item.url,
             key: item.id,
             description: item.breeds[0].description,
+            perks: {
+              adaptability: item.breeds[0].adaptability,
+              affection_level: item.breeds[0].affection_level,
+              stranger_friendly: item.breeds[0].stranger_friendly,
+              social_needs: item.breeds[0].social_needs,
+            },
           };
 
           itemData.push(tempItem);
@@ -60,33 +72,32 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col items-center mt-16">
+    <div className="flex flex-col items-center mt-16 mb-10">
       <div className="h-10 flex flex-col justify-center">
         <img
           src="https://images.vexels.com/content/132714/preview/witch-cat-face-aaba91.png"
           className="max-h-28 w-auto"
           alt="React logo"
         />
-        <h1>Buscador de Gatitos</h1>
+        <h1 className="text-center font-serif text-3xl">Gatalog</h1>
       </div>
       <div className="flex flex-col items-center mt-14">
         <button
-          className="w-28 h-10 bg-orange-600 rounded-xl m-5"
+          className="w-28 h-10 bg-orange-600 rounded-xl m-5 hover:shadow-[0_0_20px_8px_rgba(255,255,255,0.5)] transition-shadow duration-300"
           onClick={Execution}
         >
           Click me!
         </button>
-        <ul className="w-90% flex flex-grid flex-wrap justify-around">
+        <ul
+          className="2xl:w-[80%] lg:w-[95%] w-[90%] flex flex-row flex-wrap justify-around console.log();
+        "
+        >
           {itemData.map((item) => {
-            return (
-              <li key={item.key}>
-                <Catito catInfo={item} onItemClick={handleClick} />
-              </li>
-            );
+            return <Catito catInfo={item} onItemClick={handleClick} key={item.key}/>;
           })}
         </ul>
       </div>
-      {isModalOpen && <Modal item={id} onClose={setModalOpen}/>}
+      {isModalOpen && <Modal item={id} onClose={setModalOpen} />}
     </div>
   );
 }
--- src/Components/Catito/Catito.jsx
+++ src/Components/Catito/Catito.jsx
@@ -1,15 +1,62 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-const Catito = ({catInfo, onItemClick}) => {
+const Catito = ({ catInfo, onItemClick }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [value, setValue] = useState(5);
 
-    return (
-        <>
-            <div className="flex flex-col items-center p-3 border-2 m-2 cursor-pointer" onClick={() => onItemClick(catInfo)}>
-                <img className="max-h-48 w-auto" src={catInfo.image} height='1rem' alt="catito" />
-                <p>{catInfo.name}</p>
-            </div>
-        </>
-    )
+  const handleClick = (data) => {
+    // onItemClick(catInfo);
+    setIsFlipped(!isFlipped);
+  };
+
+  const formatText = (text) => {
+    return text
+      .split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+
+  return (
+    <li
+      key={catInfo.key}
+      className="w-96 m-1 h-[42.5rem] perspective-1000 cursor-pointer"
+      onClick={() => handleClick(catInfo.key)}
+    >
+      <div
+        className={`relative w-full h-full transition-transform duration-500 [transform-style:preserve-3d] ${
+          isFlipped ? "[transform:rotateY(180deg)]" : ""
+        }`}
+      >
+        {/* Front */}
+        <div className="absolute w-full h-full bg-transparent border-4 border-white text-white flex items-center flex-col justify-center rounded-lg shadow-lg [backface-visibility:hidden]">
+          <h2 className="text-3xl font-bold absolute top-5">{catInfo.name}</h2>
+          <img
+            className="absolute self-center max-w-[95%] max-h-[80%] rounded-lg"
+            src={catInfo.image}
+            alt="cat image"
+          />
+        </div>
+        {/* Back */}
+        <div className="absolute w-full h-full bg-gray-800 text-white flex flex-col items-center justify-around rounded-lg shadow-lg [backface-visibility:hidden] [transform:rotateY(180deg)] p-4">
+          <h2 className="text-2xl font-semibold">{catInfo.name}</h2>
+          <p className="text-lg text-center">{catInfo.description}</p>
+          <div className="w-full">
+            {Object.entries(catInfo.perks).map(([key, value]) => (
+              <div key={key}>
+                <h2>{formatText(key)}</h2>
+                <div className="bg-red-500 w-full h-4 rounded-lg">
+                  <div
+                    className="bg-green-500 h-4 rounded-lg"
+                    style={{ width: `${value * 10}%` }}
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default Catito;

68107f26ba41c31daea45b8918e55b036980984f
Feluiz <138745247+Feluiz@users.noreply.github.com>
2025-05-17 15:44:17 -0600

    v1.1.1 feature: Added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
+import { Outlet } from "react-router";
 import Cats from "./Components/Cats/Cats";
 
 function App() {
-  return <div>
-    <Cats />
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/Cats" element={<Cats />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
